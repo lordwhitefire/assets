74a21226a3a417bc0d38d621799001c06198f562
lordwhitefire <victormakuo2002@gmail.com>
2024-08-26 15:37:37 +0100

    added an object in the array

--- js/products.js
+++ js/products.js
@@ -1,99 +1,134 @@
 // products.js
 export const products = [
-    {
-      id: "product-001",
-      type: "https://lordwhitefire.github.io/eliphany/pages/products/skin.html",
-     
-      type_text: "Skin",
-      category_1_link: "https://lordwhitefire.github.io/eliphany/pages/products/skin.html",
-      category_1_text: "Skin",
-      category_2_link: "https://lordwhitefire.github.io/eliphany/pages/products/skin.html",
-      category_2_text: "Clothing",
-      tag_1_link: "https://lordwhitefire.github.io/eliphany/pages/products/comfort/",
-      tag_1_text: "comfort",
-      tag_2_link: "https://lordwhitefire.github.io/eliphany/pages/products/men/",
-      tag_2_text: "men",
-      tag_3_link: "https://lordwhitefire.github.io/eliphany/pages/products/modern/",
-      tag_3_text: "modern",
-      name: "Vita Optimal Age Defying Collagen Supplement",
-      image: {
-        dataThumb: "https://lordwhitefire.github.io/assets/epiphany/100/AgeDefying_Collagen-100*100.jpeg",
-        href: "https://lordwhitefire.github.io/assets/epiphany/600/AgeDefying_Collagen.jpeg",
-        src: "https://lordwhitefire.github.io/assets/epiphany/600/AgeDefying_Collagen.jpeg",
-        title: "product-1",
-        dataSrc: "https://lordwhitefire.github.io/assets/epiphany/600/AgeDefying_Collagen.jpeg",
-        dataLargeImage: "https://lordwhitefire.github.io/assets/epiphany/600/AgeDefying_Collagen.jpeg",
-        srcset: "https://lordwhitefire.github.io/assets/epiphany/300/AgeDefying_Collagen-300*300.jpeg 300w, https://lordwhitefire.github.io/assets/epiphany/600/AgeDefying_Collagen.jpeg 600w, https://lordwhitefire.github.io/assets/epiphany/100/AgeDefying_Collagen-100*100.jpeg 100w, https://lordwhitefire.github.io/assets/epiphany/150/AgeDefying_Collagen-150*150.jpeg 150w",
-      },
-      price: 0.00,
-      shortDescription: "Age Defying Collagen by Vita Optimal. Reduces visible signs of aging, providing long-lasting anti-aging benefits with sustained release technology. Enhance your skin’s youthfulness.",
-    
-      dataProductId: "001",
-      description: "Glutathione White with Mulberry and Vitamin C lightens and evens out your complexion. Anti-ageing, moisturizing, and enriched with collagen, it ensures youthful, glowing skin. Made in the USA, ensuring purity and potency."
-    },
-    {
-      id: "product-002",
-      type: "https://lordwhitefire.github.io/eliphany/pages/products/women.html",
-     
-      type_text: "Women",
-      category_1_link: "https://lordwhitefire.github.io/eliphany/pages/products/skin.html",
-      category_1_text: "Skin",
-      category_2_link: "https://lordwhitefire.github.io/eliphany/pages/products/women.html",
-      category_2_text: "Women",
-      tag_1_link: "https://lordwhitefire.github.io/eliphany/pages/products/comfort/",
-      tag_1_text: "comfort",
-      tag_2_link: "https://lordwhitefire.github.io/eliphany/pages/products/women.html",
-      tag_2_text: "women",
-      tag_3_link: "https://lordwhitefire.github.io/eliphany/pages/products/skin.html",
-      tag_3_text: "skin",
-      name: "Ladies Shine Advanced Skin Supplement",
-      image: {
-        dataThumb: "https://lordwhitefire.github.io/assets/epiphany/100/LadiesShine_Supplement-100*100.jpeg",
-        href: "https://lordwhitefire.github.io/assets/epiphany/600/LadiesShine_Supplement.jpeg",
-        src: "https://lordwhitefire.github.io/assets/epiphany/600/LadiesShine_Supplement.jpeg",
-        title: "product-2",
-        dataSrc: "https://lordwhitefire.github.io/assets/epiphany/600/LadiesShine_Supplement.jpeg",
-        dataLargeImage: "https://lordwhitefire.github.io/assets/epiphany/600/LadiesShine_Supplement.jpeg",
-        srcset: "https://lordwhitefire.github.io/assets/epiphany/300/LadiesShine_Supplement-300*300.jpeg 300w, https://lordwhitefire.github.io/assets/epiphany/600/LadiesShine_Supplement.jpeg 600w, https://lordwhitefire.github.io/assets/epiphany/100/LadiesShine_Supplement-100*100.jpeg 100w, https://lordwhitefire.github.io/assets/epiphany/150/LadiesShine_Supplement-150*150.jpeg 150w",
-  
+  {
+    id: "product-001",
+    type: "https://lordwhitefire.github.io/eliphany/pages/products/skin.html",
    
-     
-      },
-      price: 0.00,
-      shortDescription: "Ladies Shine Advanced eliminates knuckle wrinkles, provides UV protection, and smooths skin. Contains natural ingredients for overall skin enhancement and dark spot reduction.",
-    
-      dataProductId: "001",
-      description: "Ladies Shine Advanced enhances skin health. It smooths body and knuckle skin, offers UV protection, and reduces wrinkles. Natural ingredients ensure effective and gentle care."
+    type_text: "Skin",
+    category_1_link: "https://lordwhitefire.github.io/eliphany/pages/products/skin.html",
+    category_1_text: "Skin",
+    category_2_link: "https://lordwhitefire.github.io/eliphany/pages/products/skin.html",
+    category_2_text: "Clothing",
+    tag_1_link: "https://lordwhitefire.github.io/eliphany/pages/products/comfort/",
+    tag_1_text: "comfort",
+    tag_2_link: "https://lordwhitefire.github.io/eliphany/pages/products/men/",
+    tag_2_text: "men",
+    tag_3_link: "https://lordwhitefire.github.io/eliphany/pages/products/modern/",
+    tag_3_text: "modern",
+    name: "Vita Optimal Age Defying Collagen Supplement",
+    image: {
+      dataThumb: "https://lordwhitefire.github.io/assets/epiphany/100/AgeDefying_Collagen-100*100.jpeg",
+      href: "https://lordwhitefire.github.io/assets/epiphany/600/AgeDefying_Collagen.jpeg",
+      src: "https://lordwhitefire.github.io/assets/epiphany/600/AgeDefying_Collagen.jpeg",
+      title: "product-1",
+      dataSrc: "https://lordwhitefire.github.io/assets/epiphany/600/AgeDefying_Collagen.jpeg",
+      dataLargeImage: "https://lordwhitefire.github.io/assets/epiphany/600/AgeDefying_Collagen.jpeg",
+      srcset: "https://lordwhitefire.github.io/assets/epiphany/300/AgeDefying_Collagen-300*300.jpeg 300w, https://lordwhitefire.github.io/assets/epiphany/600/AgeDefying_Collagen.jpeg 600w, https://lordwhitefire.github.io/assets/epiphany/100/AgeDefying_Collagen-100*100.jpeg 100w, https://lordwhitefire.github.io/assets/epiphany/150/AgeDefying_Collagen-150*150.jpeg 150w",
     },
-    
-    {
-      id: "product-114",
-      type: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/product-category/clothing/",
-      type_text: "Clothing",
-      category_1_link: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/product-category/clothing/",
-      category_1_text: "Clothing",
-      category_2_link: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/product-category/women/",
-      category_2_text: "Women",
-      tag_1_link: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/product-tag/comfort/",
-      tag_1_text: "comfort",
-      tag_2_link: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/product-tag/fashion/",
-      tag_2_text: "fashion",
-      name: "Ladies Jeans",
-      image: {
-        dataThumb: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7-100x100.jpg",
-        href: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7.jpg",
-        src: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7-600x600.jpg",
-        title: "product-7",
-        dataSrc: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7.jpg",
-        dataLargeImage: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7.jpg",
-        srcset: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7.jpg 600w, https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7-300x300.jpg 300w, https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7-100x100.jpg 100w, https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7-150x150.jpg 150w"
-      },
-      price: "29.00",
-      shortDescription: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
-      dataProductId: "114",
-      description: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo."
-    }
+    price: 0.00,
+    shortDescription: "Age Defying Collagen by Vita Optimal. Reduces visible signs of aging, providing long-lasting anti-aging benefits with sustained release technology. Enhance your skin’s youthfulness.",
+  
+    dataProductId: "001",
+    description: "Glutathione White with Mulberry and Vitamin C lightens and evens out your complexion. Anti-ageing, moisturizing, and enriched with collagen, it ensures youthful, glowing skin. Made in the USA, ensuring purity and potency."
+  },
+  {
+    id: "product-002",
+    type: "https://lordwhitefire.github.io/eliphany/pages/products/women.html",
+   
+    type_text: "Women",
+    category_1_link: "https://lordwhitefire.github.io/eliphany/pages/products/skin.html",
+    category_1_text: "Skin",
+    category_2_link: "https://lordwhitefire.github.io/eliphany/pages/products/women.html",
+    category_2_text: "Women",
+    tag_1_link: "https://lordwhitefire.github.io/eliphany/pages/products/comfort/",
+    tag_1_text: "comfort",
+    tag_2_link: "https://lordwhitefire.github.io/eliphany/pages/products/women.html",
+    tag_2_text: "women",
+    tag_3_link: "https://lordwhitefire.github.io/eliphany/pages/products/skin.html",
+    tag_3_text: "skin",
+    name: "Ladies Shine Advanced Skin Supplement",
+    image: {
+      dataThumb: "https://lordwhitefire.github.io/assets/epiphany/100/LadiesShine_Supplement-100*100.jpeg",
+      href: "https://lordwhitefire.github.io/assets/epiphany/600/LadiesShine_Supplement.jpeg",
+      src: "https://lordwhitefire.github.io/assets/epiphany/600/LadiesShine_Supplement.jpeg",
+      title: "product-2",
+      dataSrc: "https://lordwhitefire.github.io/assets/epiphany/600/LadiesShine_Supplement.jpeg",
+      dataLargeImage: "https://lordwhitefire.github.io/assets/epiphany/600/LadiesShine_Supplement.jpeg",
+      srcset: "https://lordwhitefire.github.io/assets/epiphany/300/LadiesShine_Supplement-300*300.jpeg 300w, https://lordwhitefire.github.io/assets/epiphany/600/LadiesShine_Supplement.jpeg 600w, https://lordwhitefire.github.io/assets/epiphany/100/LadiesShine_Supplement-100*100.jpeg 100w, https://lordwhitefire.github.io/assets/epiphany/150/LadiesShine_Supplement-150*150.jpeg 150w",
+
  
+   
+    },
+    price: 0.00,
+    shortDescription: "Ladies Shine Advanced eliminates knuckle wrinkles, provides UV protection, and smooths skin. Contains natural ingredients for overall skin enhancement and dark spot reduction.",
   
-  ];
+    dataProductId: "002",
+    description: "Ladies Shine Advanced enhances skin health. It smooths body and knuckle skin, offers UV protection, and reduces wrinkles. Natural ingredients ensure effective and gentle care."
+  },
   
+  {
+    id: "product-003",
+    type: "https://lordwhitefire.github.io/eliphany/pages/products/women.html",
+   
+    type_text: "Women",
+    category_1_link: "https://lordwhitefire.github.io/eliphany/pages/products/skin.html",
+    category_1_text: "Skin",
+    category_2_link: "https://lordwhitefire.github.io/eliphany/pages/products/women.html",
+    category_2_text: "Women",
+    tag_1_link: "https://lordwhitefire.github.io/eliphany/pages/products/comfort/",
+    tag_1_text: "comfort",
+    tag_2_link: "https://lordwhitefire.github.io/eliphany/pages/products/women.html",
+    tag_2_text: "women",
+    tag_3_link: "https://lordwhitefire.github.io/eliphany/pages/products/beauty.html",
+    tag_3_text: "skin",
+    name: "MagicBrightening_Supplement",
+    image: {
+      dataThumb: "https://lordwhitefire.github.io/assets/epiphany/100/MagicBrightening_Supplement-100*100.jpeg",
+      href: "https://lordwhitefire.github.io/assets/epiphany/600/MagicBrightening_Supplement.jpeg",
+      src: "https://lordwhitefire.github.io/assets/epiphany/600/MagicBrightening_Supplement.jpeg",
+      title: "product-2",
+      dataSrc: "https://lordwhitefire.github.io/assets/epiphany/600/MagicBrightening_Supplement.jpeg",
+      dataLargeImage: "https://lordwhitefire.github.io/assets/epiphany/600/MagicBrightening_Supplement.jpeg",
+      srcset: "https://lordwhitefire.github.io/assets/epiphany/300/MagicBrightening_Supplement-300*300.jpeg 300w, https://lordwhitefire.github.io/assets/epiphany/600/MagicBrightening_Supplement.jpeg 600w, https://lordwhitefire.github.io/assets/epiphany/100/MagicBrightening_Supplement-100*100.jpeg 100w, https://lordwhitefire.github.io/assets/epiphany/150/MagicBrightening_Supplement-150*150.jpeg 150w",
+
+
+
+   
+   },
+    price: 0.00,
+    shortDescription: "Magic Brightening with Glutathione, Birdnest, and Collagen Peptide. Achieve brighter, youthful skin with long-lasting whitening effects. Includes UV protection and bee honey for enhanced results.",
+  
+    dataProductId: "003",
+    description: "Boost your skin's radiance with Magic Brightening. Contains Glutathione, Birdnest, and Collagen Peptide for smoother, fairer skin. Provides UV protection and includes natural bee honey extract."
+  },
+  
+  {
+    id: "product-114",
+    type: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/product-category/clothing/",
+    type_text: "Clothing",
+    category_1_link: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/product-category/clothing/",
+    category_1_text: "Clothing",
+    category_2_link: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/product-category/women/",
+    category_2_text: "Women",
+    tag_1_link: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/product-tag/comfort/",
+    tag_1_text: "comfort",
+    tag_2_link: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/product-tag/fashion/",
+    tag_2_text: "fashion",
+    name: "Ladies Jeans",
+    image: {
+      dataThumb: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7-100x100.jpg",
+      href: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7.jpg",
+      src: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7-600x600.jpg",
+      title: "product-7",
+      dataSrc: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7.jpg",
+      dataLargeImage: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7.jpg",
+      srcset: "https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7.jpg 600w, https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7-300x300.jpg 300w, https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7-100x100.jpg 100w, https://www.prodesigns.com/wordpress-themes/demo/ecommerce-gem/wp-content/uploads/sites/16/2018/01/product-7-150x150.jpg 150w"
+    },
+    price: "29.00",
+    shortDescription: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
+    dataProductId: "114",
+    description: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo."
+  }
+
+
+];
